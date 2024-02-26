7f2b0dbe257f6e2390d87461acc05316f93b7bf7
Priya Kumari <119745288+PriyaKumari122@users.noreply.github.com>
2024-02-26 22:34:50 +0530

    good_string.js

---
+++ good_string.js
@@ -0,0 +1,10 @@
+// a good string is a string that starts with the letter 'a' & and has length >= 5
+
+let word  = "apple" ;
+
+if ((word[0] == 'a' ) && (word.length >=5) ){
+    console.log(`Good String`);
+}
+else {
+    console.log(`Not a Good String`) ;
+}
